1dd0dc48cb777f595ada68b5d3f791830742e271
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-10-13 15:57:48 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-// inicializador da aplicação
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const rotaPsicologos = require('./routes/psicologosRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/psicologos', rotaPsicologos);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
