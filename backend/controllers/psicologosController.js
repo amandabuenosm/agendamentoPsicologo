c0b944956b934c01bb7e96fc5030eccc34486540
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-10-13 15:56:24 -0300

    Update psicologosController.js

--- backend/controllers/psicologosController.js
+++ backend/controllers/psicologosController.js
@@ -1,1 +1,47 @@
-// funções CRUD psicólogos
+const modelPsicologo = require('../models/psicologosModel');
+
+const controllerPsicologo = {
+
+    mostrarPsicologos: (req, res) => {
+        modelPsicologo.mostrarPsicologos((err, results) => {
+            if (err) return res.status(500).json({ erro: err });
+            res.json(results);
+        });
+    },
+
+    cadastrarPsicologo: (req, res) => {
+        const novoPsicologo = req.body;
+        modelPsicologo.cadastrarPsicologo(novoPsicologo, (err, result) => {
+            if (err) return res.status(500).json({ erro: err });
+            res.status(201).json({ id: result.insertId, ...novoPsicologo });
+        });
+    },
+
+    editarPsicologo: (req, res) => {
+        const id = req.params.id;
+        const dadoseditados = req.body;
+        modelPsicologo.editarPsicologo(id, dadoseditados, (err) => {
+            if (err) return res.status(500).json({ erro: err });
+            res.json({ mensagem: 'Dados do psicólogo atualizados com sucesso!' });
+        });
+    },
+
+    trocarstatus: (req, res) => {
+        const id = req.params.id;
+        const { status } = req.body;
+        modelPsicologo.trocarstatus(id, status, (err) => {
+            if (err) return res.status(500).json({ erro: err });
+            res.json({ mensagem: 'Status do psicólogo atualizado com sucesso!' });
+        });
+    },
+    
+    deletarPsicologo: (req, res) => { 
+        const id = req.params.id;
+        modelPsicologo.deletarPsicologo(id, (err) => {
+            if (err) return res.status(500).json({ erro: err});
+            res.json({ mensagem: 'Psicólogo excluído com sucesso!' });
+        });
+    }
+};
+
+module.exports = controllerPsicologo;
