c9baf986d3eba8b26df4f98853660695f948dc7b
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-10-13 15:56:52 -0300

    Update psicologosModel.js

--- backend/models/psicologosModel.js
+++ backend/models/psicologosModel.js
@@ -1,1 +1,26 @@
-// funções CRUD de psicólogos com o banco de dados
+const database = require('../config/database');
+
+const crudPsicologos = {
+
+    mostrarPsicologos: (callback) => {
+        database.query('SELECT * FROM psicologos', callback);
+    },
+
+    cadastrarPsicologo: (novoPsicologo, callback) => {
+        database.query('INSERT INTO psicologos SET ?', novoPsicologo, callback);
+    },
+
+    editarPsicologo: (id, psicologoEdit, callback) => {
+        database.query('UPDATE psicologos SET ? WHERE id = ?', [psicologoEdit, id], callback);
+    },
+
+    trocarstatus: (id, status, callback) => {
+        database.query('UPDATE psicologos SET status = ? WHERE id = ?', [status, id], callback);
+    },
+
+    deletarPsicologo: (id, callback) => {
+        database.query('DELETE FROM psicologos WHERE id = ?', [id], callback);
+    },
+}
+
+module.exports = crudPsicologos;
