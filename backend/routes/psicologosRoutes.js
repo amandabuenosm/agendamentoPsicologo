806842342af6db9ddce55291ca8dddad3316f849
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-10-13 15:57:22 -0300

    Update psicologosRoutes.js

--- backend/routes/psicologosRoutes.js
+++ backend/routes/psicologosRoutes.js
@@ -1,1 +1,11 @@
-// função de direcionamento de rotas para psicólogos
+const express = require('express');
+const rota = express.Router();
+const controller = require('../controllers/psicologosController');
+
+rota.get('/', controller.mostrarPsicologos);
+rota.post('/', controller.cadastrarPsicologo);
+rota.put('/:id', controller.editarPsicologo);
+rota.put('/:id', controller.trocarstatus);
+rota.delete('/:id', controller.deletarPsicologo);
+
+module.exports = rota;
