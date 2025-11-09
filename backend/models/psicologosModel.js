452499591eba4f5d7c43da050c620d354d7e4031
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-11-09 00:31:10 -0300

    Refactor psicologosModel to use Sequelize ORM

--- backend/models/psicologosModel.js
+++ backend/models/psicologosModel.js
@@ -1,26 +1,96 @@
-const database = require('../config/database');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcrypt');
 
-const crudPsicologos = {
-
-    mostrarPsicologos: (callback) => {
-        database.query('SELECT * FROM psicologos', callback);
+const Psicologo = sequelize.define('Psicologo', {
+    id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
     },
-
-    cadastrarPsicologo: (novoPsicologo, callback) => {
-        database.query('INSERT INTO psicologos SET ?', novoPsicologo, callback);
+    nomePsicologo: {
+    type: DataTypes.STRING,
+    allowNull: false
     },
-
-    editarPsicologo: (id, psicologoEdit, callback) => {
-        database.query('UPDATE psicologos SET ? WHERE id = ?', [psicologoEdit, id], callback);
+    cpfPsicologo: {
+    type: DataTypes.STRING(14),
+    allowNull: false,
+    unique: true
     },
-
-    trocarstatus: (id, status, callback) => {
-        database.query('UPDATE psicologos SET status = ? WHERE id = ?', [status, id], callback);
+    emailPsicologo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+        isEmail: true
+    }
     },
-
-    deletarPsicologo: (id, callback) => {
-        database.query('DELETE FROM psicologos WHERE id = ?', [id], callback);
+    telefonePsicologo: {
+    type: DataTypes.STRING,
+    allowNull: false
     },
-}
+    idadePsicologo: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+    },
+    crp: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+    },
+    estadoCrp: {
+    type: DataTypes.STRING(2),
+    allowNull: false
+    },
+    especialidades: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+    formacao: {
+    type: DataTypes.TEXT,
+    allowNull: false
+    },
+    experiencia: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+    },
+    modalidadeAtendimento: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+    valorConsulta: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+    },
+    localAtendimento: {
+    type: DataTypes.TEXT,
+    allowNull: false
+    },
+    senhaPsicologo: {
+    type: DataTypes.STRING,
+    allowNull: false
+    }
+}, { hooks: { // criptografia de senhas
+        beforeCreate: async (psicologo) => {
+            if (psicologo.senhaPsicologo) {
+            psicologo.senhaPsicologo = await bcrypt.hash(psicologo.senhaPsicologo, 10);
+            }
+        },
+        beforeUpdate: async (psicologo) => {
+            if (psicologo.changed('senha') && psicologo.senhaPsicologo) {
+            psicologo.senhaPsicologo = await bcrypt.hash(psicologo.senhaPsicologo, 10);
+            }
+        }
+    }
+});
 
-module.exports = crudPsicologos;
+Psicologo.prototype.validarSenha = function(senhaPsicologo) { // validar senha
+  return bcrypt.compare(senhaPsicologo, this.senhaPsicologo);
+};
+
+Psicologo.prototype.atualizarSenha = async function(novaSenhaPsicologo) { // atualizar senha
+  this.senhaPsicologo = novaSenhaPsicologo;
+  return await this.save();
+};
+
+module.exports = Psicologo;
