0893e9d8f27ba88a537f147be99a1aa64c3e5da5
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-12-01 13:34:53 -0300

    Delete backend directory
    Serão adicionados novos arquivos

--- backend/routes/consultasRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const rota = express.Router();
-const consultasController = require('../controllers/consultasController');
-
-rota.post('/agendar', consultasController.agendarConsulta);
-
-module.exports = rota;
