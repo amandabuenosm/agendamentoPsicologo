d7bd40dff24fda2e4cf5eb7ad23a7da98a738bd1
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-12-01 13:47:38 -0300

    Delete frontend directory
    Serão adicionados outros arquivos

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// instância do Axios
-const api = axios.create({
-  baseURL: 'http://localhost:3000/',
-  timeout: 10000, // tempo de espera para requisição (10 segundos)
-});
-
-export default api;
